1f7645fa5f4e3a8d6a6d561eba7e06635662e1fa
vitaliy kotov <v.kotov@corp.badoo.com>
2021-09-26 11:10:51 +0300

    First step

---
+++ src/test/java/tests/SearchTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import lib.CoreTestCase;
+import org.junit.Test;
+
+public class SearchTest extends CoreTestCase {
+
+    @Test
+    public void testFindJava() {
+        System.out.println("Success");
+    }
+}
